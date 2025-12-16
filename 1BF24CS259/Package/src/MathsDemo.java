a64ad4129a0498cb86431a9eb5e1450007014896
Saachi R Katti <saachirkatti.cs24@bmsce.ac.in>
2025-12-16 12:08:20 +0530

    Add files via upload

---
+++ 1BF24CS259/Package/src/MathsDemo.java
@@ -0,0 +1,9 @@
+package Package.src;
+import Maths.Number;
+public class MathsDemo {
+    public static void main(String[] args){
+        Number n = new Number();
+        n.add();
+        n.subtract();
+    }
+}
