448d3c4188e604caa88cbab497b3ca23a3842560
Saachi R Katti <saachirkatti.cs24@bmsce.ac.in>
2025-12-16 12:07:52 +0530

    Delete 1BF24CS259/Package directory

--- 1BF24CS259/Package/MathsDemo.java
+++
@@ -1,9 +0,0 @@
-package Package.src;
-import Maths.Number;
-public class MathsDemo {
-    public static void main(String[] args){
-        Number n = new Number();
-        n.add();
-        n.subtract();
-    }
-}
